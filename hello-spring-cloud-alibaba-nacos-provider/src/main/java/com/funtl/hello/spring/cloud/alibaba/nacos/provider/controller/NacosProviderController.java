d65664e18f796319f30367e6bcdb670ec0e87919
yanlp <85827444@qq.com>
2019-11-09 17:17:34 +0800

    Nacos Config 服务端和客户端的使用

--- hello-spring-cloud-alibaba-nacos-provider/src/main/java/com/funtl/hello/spring/cloud/alibaba/nacos/provider/controller/NacosProviderController.java
+++ hello-spring-cloud-alibaba-nacos-provider/src/main/java/com/funtl/hello/spring/cloud/alibaba/nacos/provider/controller/NacosProviderController.java
@@ -1,6 +1,8 @@
 package com.funtl.hello.spring.cloud.alibaba.nacos.provider.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,11 +19,20 @@ public class NacosProviderController {
 
     @Value("${server.port}")
     private String port;
+    @Autowired
+    private ConfigurableApplicationContext configurableApplicationContext;
 
 
     @GetMapping("/echo/{message}")
     public String echo(@PathVariable("message") String message) {
+
         return "Hello Nacos " +  message + " i am from " + port;
     }
-//
+
+    @GetMapping("/hi")
+    public String hi() {
+        return configurableApplicationContext.getEnvironment().getProperty("user.name");
+    }
+
+
 }
